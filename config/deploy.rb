b7da50250d3cb9704a8f72ef7c15915c95c8c4a4
John Topley <john@johntopley.com>
2008-07-28 19:51:14 +0100

    Changed deploy.rb to use domain name instead of IP address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 default_run_options[:pty] = true
 set :application, 'addresswatcher'
-set :domain,      '209.20.82.43'
+set :domain,      'addresswatcher.com'
 set :user,        'deploy'
 set :repository,  'git@github.com:johntopley/address-watcher.git'
 set :scm,         'git'
