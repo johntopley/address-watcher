8c0a036f912c17bf3875316e6387fecd2de2c80e
John Topley <john@johntopley.com>
2008-07-28 07:49:41 +0100

    Including ExceptionNotifiable in ApplicationController

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,6 +3,7 @@
 
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
+  include ExceptionNotifiable
   helper :all # include all helpers, all the time
   filter_parameter_logging :password
   
