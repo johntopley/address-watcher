60b6e6139ba845d333d77551e6f9a121f14f756a
John Topley <john@johntopley.com>
2008-07-29 20:11:50 +0100

    Removed empty tests and removed unpacked twitter gem

--- test/unit/watch_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class WatchTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
--- vendor/gems/twitter-0.2.7/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-gem 'rspec'
-require 'spec'
-
-dir = File.dirname(__FILE__)
-
-$:.unshift(File.join(dir, '/../lib/'))
-require dir + '/../lib/twitter'
